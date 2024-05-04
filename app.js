14040efaff89c03d0dc2f52bda080e75afdb09e6
muhammadkashif20 <kashif28@gmail.com>
2024-05-03 22:12:57 -0700

    compltetd

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var listofIntegers=['1','2','3','4','5','6','7','8','9','10']
 for(var i=1;i<=listofIntegers.length;i++){
     if(listofIntegers[i]%3===0){
-        console.log(`${arr[i]},Three`);
+        console.log(`${listofIntegers[i]},Three`);
     }
     else if(listofIntegers[i]%5===0){
-        console.log(`${arr[i]},Three`);
+        console.log(`${listofIntegers[i]},Five`);
     }
     else if(listofIntegers[i]%3===0 && listofIntegers[i]%5===0){
-        console.log(`${arr[i]},ThreeFive`);
+        console.log(`${listofIntegers[i]},ThreeFive`);
     }
     else{
         console.log('List is not divisible');
